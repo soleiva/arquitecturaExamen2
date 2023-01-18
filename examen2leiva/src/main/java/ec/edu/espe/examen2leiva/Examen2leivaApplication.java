e0b82778e6c9651bc5f2a8f0899d1522d884e310
Steven Leiva <stevenomar.leiva@gmail.com>
2023-01-18 10:13:08 -0500

    Create exam

---
+++ examen2leiva/src/main/java/ec/edu/espe/examen2leiva/Examen2leivaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examen2leiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen2leivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen2leivaApplication.class, args);
+	}
+
+}
---
+++ examen2leiva/src/test/java/ec/edu/espe/examen2leiva/Examen2leivaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examen2leiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2leivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
