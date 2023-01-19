f1a908954909691002d6c7b3e0d83fea20fcbcf6
Steven Leiva <stevenomar.leiva@gmail.com>
2023-01-18 21:23:55 -0500

    Exam 2

--- examen2leiva/src/main/java/ec/edu/espe/examen2leiva/model/Turno.java
+++ examen2leiva/src/main/java/ec/edu/espe/examen2leiva/model/Turno.java
@@ -6,11 +6,15 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.index.Indexed;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Turno {
     @Id
     private String id;
--- examen2leiva/src/main/java/ec/edu/espe/examen2leiva/service/TurnoService.java
+++ examen2leiva/src/main/java/ec/edu/espe/examen2leiva/service/TurnoService.java
@@ -1,27 +1,27 @@
 package ec.edu.espe.examen2leiva.service;
 
 import java.util.Date;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import ec.edu.espe.examen2leiva.model.Ejecutivo;
 import ec.edu.espe.examen2leiva.model.SolicitudTurno;
 import ec.edu.espe.examen2leiva.model.Turno;
+import ec.edu.espe.examen2leiva.repository.EjecutivoRespository;
 import ec.edu.espe.examen2leiva.repository.SolicitudTurnoRepository;
 import ec.edu.espe.examen2leiva.repository.TurnoRespository;
 
 @Service
 public class TurnoService {
     private final TurnoRespository turnoRespository;
-   
+    private final EjecutivoRespository ejecutivoRespository;
 
-    
 
-    public TurnoService(TurnoRespository turnoRespository) {
+    public TurnoService(TurnoRespository turnoRespository, EjecutivoRespository ejecutivoRespository) {
         this.turnoRespository = turnoRespository;
+        this.ejecutivoRespository = ejecutivoRespository;
     }
-
-
 
     public void guardarTurno(Turno turno) {
         SolicitudTurno solicitudTurno;
@@ -44,6 +44,26 @@ public class TurnoService {
             throw e;
         }
 
+    }
+
+    public void iniciAtencion(String numTurno, String codUsuarioEjecutivo) {
+        Turno optional = this.turnoRespository.findByNumTurnoAndCodUsuaioEjecutivo(numTurno,
+                codUsuarioEjecutivo);
+        Ejecutivo optEjecutivo = this.ejecutivoRespository.findByCodigo(codUsuarioEjecutivo);
+        if (optional != null && optEjecutivo != null) {
+            
+            optional.setFechaCreacion(new Date());;
+            Ejecutivo ejecutivo = optEjecutivo.get();
+            ejecutivo.setAsignado(false);
+            try {
+                this.turnoRepository.save(turno);
+                this.ejecutivoRepository.save(ejecutivo);
+            } catch (Exception e) {
+                throw new CustomRuntimeException("Ha ocurrido un error", ResponseCodeUtils.INTERNAL_ERROR_SERVER.code);
+            }
+        } else {
+            throw new CustomRuntimeException("El turno no ha sido encontrado", ResponseCodeUtils.NOT_FOUND.code);
+        }
     }
 
     
